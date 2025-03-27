3eb4157ad342d0011509b53dd8f90fcd83403ad0
calberto <rinoa_heartili@hotmail.com>
2025-03-27 13:25:46 -0600

    Se corrige problema en counter screen

--- lib/presentation/screen/counter/counter_screen.dart
+++ lib/presentation/screen/counter/counter_screen.dart
@@ -16,7 +16,7 @@ class CounterScreen extends ConsumerWidget
   Widget build(BuildContext context, WidgetRef ref) 
   {
     final int clickCounter = ref.watch(counterProvider);
-    final bool isDarkmode = ref.watch(isDarkmodeProvider);
+    final bool isDarkmode = ref.watch(themeNotifierProvider).isDarkmode;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
@@ -24,7 +24,8 @@ class CounterScreen extends ConsumerWidget
           IconButton(
             icon: Icon(isDarkmode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
             onPressed: (){
-              ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+              //ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+              ref.read(themeNotifierProvider.notifier).toggleDarkmode();
             }, 
             
           ),
